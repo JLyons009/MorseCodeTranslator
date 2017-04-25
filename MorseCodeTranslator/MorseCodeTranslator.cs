0e7df2ba4fc940802bef86ab016e0d7dfb5dda05
JLyons009 <JLyons009@gmail.com>
2017-04-25 14:58:04 -0500

    Initial setup.  Added Dictionary to MorseCodeTranslator as well as ToMorse(string input) static method.

---
+++ MorseCodeTranslator/MorseCodeTranslator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeTranslator
+{
+    static class MorseCodeTranslator
+    {
+        private static Dictionary<char, string> _textToMorse = new Dictionary<char, string>
+        {
+            {' ', "/"},
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."},
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."},
+            {'0', "-----"},
+            {',', "--..--"},
+            {'.', ".-.-.-"},
+            {'?', "..--.."},
+            {';', "-.-.-."},
+            {':', "---..."},
+            {'/', "-..-."},
+            {'-', "-....-"},
+            {'\'', ".----."},
+            {'"', ".-..-."},
+            {'(', "-.--."},
+            {')', "-.--.-"},
+            {'=', "-...-"},
+            {'+', ".-.-."},
+            {'@', ".--.-."},
+            {'!', "-.-.--"},
+            {'Á', ".--.-"},
+            {'É', "..-.."},
+            {'Ö', "---."},
+            {'Ä', ".-.-"},
+            {'Ñ', "--.--"},
+            {'Ü', "..--"}
+        };
+
+        public static string ToMorse(string input)
+        {
+            List<string> output = new List<string>(input.Length);
+
+            foreach (char character in input.ToUpper())
+            {
+                try
+                {
+                    output.Add(_textToMorse[character]);
+                }
+                catch(KeyNotFoundException)
+                {
+                    output.Add("#");
+                }
+            }
+
+            return string.Join(" ", output);
+        }
+    }
+}
