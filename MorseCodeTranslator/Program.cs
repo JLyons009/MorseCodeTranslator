470a78e9b9d168405411607e94ddf972af3d7b34
JLyons009 <JLyons009@gmail.com>
2017-04-25 15:05:03 -0500

    Added user input that outputs morse code and repeats until user quits.

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -10,6 +10,18 @@ namespace MorseCodeTranslator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("String to translate to Morse Code (enter nothing to quit): ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Morse Code: " + MorseCodeTranslator.ToMorse(input));
+            }
         }
     }
 }
